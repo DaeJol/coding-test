dc225fce77abad55b44fd3d59fc43293e2eca97b
keykat <koin12000@naver.com>
2024-04-19 23:43:19 +0900

    Update 258711.py

--- 프로그래머스/258711.py
+++ 프로그래머스/258711.py
@@ -1,13 +1,27 @@
 N = int(input())
 towers = list(map(int, input().split(' ')))
-dp = [0 for _ in range(N)]
+result = [0 for _ in range(N)]
+# [인덱스, 높이]
+stack = []
 
-for idx, tower in enumerate(towers):
-    # print(towers[idx + 1 :])
-    for idx2, tower2 in enumerate(towers[idx + 1:], idx + 1):
-        
-        if tower > tower2: 
-            dp[idx2] = idx + 1
-        # print(idx2, tower, tower2, towers[idx + 1 : ], dp)
+# towers.reverse() --> void
+# towers = reversed(towers)
+# towers = towsers[::-1]
+towers.reverse()
+for idx in range(len(towers)):
+    curr = towers[idx]
+    if not stack:
+        stack.append([idx, curr])
+    else:
+        while stack:
+            top = stack[-1]
+            if top[1] < curr:
+                result[top[0]] = len(towers) - idx
+                stack.pop()
+            else:
+                break
 
-print(' '.join(str(ele) for ele in dp))
+        stack.append([idx, curr]) 
+    # print('result:', result)
+
+print(' '.join(str(e) for e in reversed(result)))
