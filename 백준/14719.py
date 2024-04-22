602711db1abc18385db7a9b01d3b74db4bd9f4be
keykat <koin12000@naver.com>
2024-04-22 23:27:11 +0900

    Update 14719.py

--- 백준/14719.py
+++ 백준/14719.py
@@ -1,31 +1,18 @@
-# 4 4
-# 3 0 1 4
+# # 4 4
+# # 3 0 1 4
 
 H, W = list(map(int, input().split(' ')))
 pillows = list(map(int, input().split(' ')))
 
-stack = []
-curr = -1
 cnt = 0
-for pillow in pillows:
-    if curr == -1:
-        if pillow > 0:
-            curr = pillow
-    else:
-        print(pillow, curr, stack)
-        # 현재 기둥이 맨처음의 기준 기둥보다 높거나 같으면 stack에서 전부 빼서 높이 계산해준다.
-        if pillow >= curr:
-            while stack:
-                cnt += (curr - stack.pop())
-            curr = -1
-        else:
-            stack.append(pillow)
-        print('cnt: ', cnt)
-
-print(curr, stack)
-if stack and stack[-1] > 0:
-    s = stack.pop()
-    while stack:
-        cnt += s - stack.pop()
+for idx in range(1, len(pillows) - 1):
+    left = max(pillows[ : idx])
+    right = max(pillows[idx + 1 :])
+    height = min(left, right)
+    if height > pillows[idx]:
+        cnt += height - pillows[idx]
 
 print(cnt)
+
+# 4 8
+# 3 1 2 3 4 1 1 2 1 1 2
