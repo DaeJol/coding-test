089126de986ae2eb8eee61d262cdaeaa353a3070
betterafter <koin12000@naver.com>
2024-05-17 01:39:28 +0900

    Update 2824.py

--- 백준/2824.py
+++ 백준/2824.py
@@ -1,3 +1,8 @@
+# 백준 2824. 최대공약수
+# https://www.acmicpc.net/problem/2824
+
+# 유클리드 호제법
+# https://namu.wiki/w/%EC%9C%A0%ED%81%B4%EB%A6%AC%EB%93%9C%20%ED%98%B8%EC%A0%9C%EB%B2%95
 def gcd(a, b):
     while b > 0:
         next = a % b
@@ -6,6 +11,8 @@ def gcd(a, b):
     return a
 
 def mul(lst) :
+    # eval: string 수식을 실제 연산으로 적용해준다.
+    # '10*20*30' 이라고 string으로 되어있는 수식을 읽어서 실제 6000의 결과값을 내줌
     return eval('*'.join([str(n) for n in lst]))
 
 
