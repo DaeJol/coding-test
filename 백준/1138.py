667d0a07a3c7d1fa024833a4f147f83d318f2f2d
betterafter <koin12000@naver.com>
2024-05-19 00:04:07 +0900

    Update 1138.py

--- 백준/1138.py
+++ 백준/1138.py
@@ -3,5 +3,38 @@
 
 # https://www.acmicpc.net/problem/1138
 
+def check(currList, li):
+    for idx, curr in enumerate(currList):
+        currCheck = 0
+        for i in range(idx):
+            if curr < currList[i]:
+                currCheck += 1
+        
+        if li[curr - 1] != currCheck:
+            return False
+    
+    return True
+
+result = []
+def com(currList, visited):
+    global N
+    global li
+    global result
+
+    if len(currList) == N:
+        if check(currList, li):
+            result = currList.copy()
+
+    for i in range(1, N + 1):
+        if visited[i] == False:
+            visited[i] = True
+            currList.append(i)
+            com(currList= currList, visited= visited)
+            visited[i] = False
+            currList.pop()
+
 N = int(input())
 li = list(map(int, input().split()))
+
+com(currList = [], visited = [False for _ in range(N + 1)])
+print(' '.join(map(str, result)))
